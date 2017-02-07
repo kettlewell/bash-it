ac9f939c2f317b55610dd74cb387342da435ef73
Matt Kettlewell <matt@kettlewell.net>
2017-02-07 08:28:28 -0600

    Added blank custom files

---
+++ custom/work.bash
@@ -0,0 +1,19 @@
+# Set Aliases
+#if [ -f ~/.aliases.mac ]; then
+#    . ~/.aliases.mac
+#fi
+
+#if [ -f $(brew --prefix)/etc/bash_completion ]; then
+#    . $(brew --prefix)/etc/bash_completion
+#fi
+
+#export CLICOLOR=1
+#export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+
+# Set Bash Prompt
+
+#if [ -f ~/.bash_ps1_git_1.sh ]; then
+#    . ~/.bash_ps1_git_1.sh
+#fi
